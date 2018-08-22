b8cd98d7c6296c8adc9a22241ac33130dd692135
E.Z. Hart <hartez@gmail.com>
2018-08-22 09:27:24 -0600

    Updated code from https://github.com/xamarin/Xamarin.Forms/issues/3616

--- SkiaSharpForms/SkiaSharpFormsDemos/SkiaSharpFormsDemos/SkiaSharpFormsDemos/Basics/SimpleCirclePage.cs
+++ SkiaSharpForms/SkiaSharpFormsDemos/SkiaSharpFormsDemos/SkiaSharpFormsDemos/Basics/SimpleCirclePage.cs
@@ -11,9 +11,45 @@ namespace SkiaSharpFormsDemos.Basics
         {
             Title = "Simple Circle";
 
-            SKCanvasView canvasView = new SKCanvasView();
+            SKCanvasView canvasView = new SKCanvasView()
+            {
+                HeightRequest = 50,
+                WidthRequest = 50,
+                VerticalOptions = LayoutOptions.End,
+                HorizontalOptions = LayoutOptions.Center
+            };
             canvasView.PaintSurface += OnCanvasViewPaintSurface;
-            Content = canvasView;
+
+            StackLayout sl = new StackLayout()
+            {
+                Orientation = StackOrientation.Vertical
+            };
+            sl.Children.Add(canvasView);
+            sl.Children.Add(new Label()
+            {
+                Text = "This Is My Circle"
+            });
+
+            Frame frame = new Frame()
+            {
+                Margin = new Thickness(0, 50, 0, 0),
+                HeightRequest = 100,
+                HorizontalOptions = LayoutOptions.Center,
+                Padding = new Thickness(0),
+                HasShadow = true
+            };
+            frame.Content = sl;
+
+            StackLayout mainStack = new StackLayout() {
+                Orientation = StackOrientation.Vertical
+            };
+            mainStack.Children.Add(frame);
+            mainStack.Children.Add(new Button()
+            {
+                Text="Click Here"
+            });
+
+            Content = mainStack;
         }
 
         void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
@@ -28,13 +64,15 @@ namespace SkiaSharpFormsDemos.Basics
             {
                 Style = SKPaintStyle.Stroke,
                 Color = Color.Red.ToSKColor(),
-                StrokeWidth = 25
+                IsAntialias = true,
+                StrokeWidth = 1
             };
-            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+            
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, info.Height / 3, paint);
 
             paint.Style = SKPaintStyle.Fill;
             paint.Color = SKColors.Blue;
-            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, info.Height / 3, paint);
         }
     }
 }
